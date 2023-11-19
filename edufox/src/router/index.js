cad92f60d807ae6d7927a2a72f6391a043741f17
AfrizalAnwar <wewsirizal@gmail.com>
2023-11-19 13:25:12 +0700

    ADD: - Logo svg - Courses By Id - See All Route in Home view in Bottom of content
    ERROR:
    - Routes: modified from "/courses/:id" to "/coursess" only for develope
    
    Additional:
    <div>course {{ route.params }}</div>
    being commented on in CourseById

--- edufox/src/router/index.js
+++ edufox/src/router/index.js
@@ -26,7 +26,7 @@ const router = createRouter({
           props: route => ({ query: route.query }),
         },
         {
-          path: "/courses/:id",
+          path: "/coursess",
           name: "course-detail",
           component: () => import("../views/CourseByIdView.vue")
         }
