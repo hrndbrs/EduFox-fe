8c9f08550c85ab35089da35f1d39f84707897139
AfrizalAnwar <wewsirizal@gmail.com>
2023-11-20 13:17:34 +0700

    ADD: - Component NotFound - Add route for enroll id - feedback&rate in courseId - endrollmentId Page
    FIX:
    class="h-screen overflow-y-scroll" in mainLayout

--- edufox/src/views/CourseByIdView.vue
+++ edufox/src/views/CourseByIdView.vue
@@ -3,7 +3,7 @@ export default {
   name: 'CourseByIdView',
   data() {
     return {
-      panel: [0, 1, 2, 3],
+      panel: [0, 1, 2, 3] 
     }
   }
 }
@@ -68,6 +68,83 @@ export default {
             </v-expansion-panel>
           </v-expansion-panels>
         </div>
+        <div>
+          <v-card
+            class="d-flex flex-column mx-auto py-8 mt-5"
+            elevation="4"
+            height="500"
+            width="420"
+          >
+            <div class="d-flex justify-center mt-auto text-h5">Rating overview</div>
+            <div class="d-flex align-center flex-column my-auto">
+              <div class="text-h2 mt-5">
+                3.5
+                <span class="text-h6 ml-n3">/5</span>
+              </div>
+
+              <v-rating :model-value="3.5" color="yellow-darken-3" half-increments></v-rating>
+              <div class="px-3">3,360 ratings</div>
+            </div>
+
+            <v-list bg-color="transparent" class="d-flex flex-column-reverse" density="compact">
+              <v-list-item v-for="(rating, i) in 5" :key="i">
+                <v-progress-linear
+                  :model-value="rating * 15"
+                  class="mx-n5"
+                  color="yellow-darken-3"
+                  height="20"
+                  rounded
+                ></v-progress-linear>
+
+                <template v-slot:prepend>
+                  <span>{{ rating }}</span>
+                  <v-icon icon="mdi-star" class="mx-3"></v-icon>
+                </template>
+
+                <template v-slot:append>
+                  <div class="rating-values">
+                    <span class="d-flex justify-end"> {{ rating * 224 }} </span>
+                  </div>
+                </template>
+              </v-list-item>
+            </v-list>
+          </v-card>
+          <div class="h-[55vh] bg-gray-100 flex items-center justify-center">
+            <div class="px-5">
+              <div
+                class="bg-white max-w-xl rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500"
+              >
+                <div
+                  class="w-14 h-14 bg-yellow-500 rounded-full flex items-center justify-center font-bold text-white"
+                >
+                  LOGO
+                </div>
+                <div class="mt-4">
+                  <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer">
+                    Product Review
+                  </h1>
+                  <div class="flex mt-2">
+                    <v-rating
+                  v-model="rating"
+                  readonly
+                  half-increments
+                  :model-value="5"
+                  :size="25"
+                  color="orange-lighten-1"
+                  class="mt-2"
+                ></v-rating>
+                  </div>
+                  <p class="mt-4 text-md text-gray-600">
+                    But I must explain to you how all this mistaken idea of denouncing pleasure and
+                    praising pain was born and I will give you a complete account of the system, and
+                    expound the actual teachings of the great explorer of the truth, the
+                    master-builder of human happines.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </main>
@@ -75,6 +152,6 @@ export default {
 
 <style scoped>
 #bd {
-  border: 0px solid red;
+  border: 1px solid red;
 }
 </style>
